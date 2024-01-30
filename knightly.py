67284177d1ff36c75da34dc2cff332bb65be1ace
Ryan James OSullivan <rjo@rynet.com>
2024-01-29 22:11:17 -0500

    bot base code in place

--- knightly.py
+++ knightly.py
@@ -1,6 +1,16 @@
 import discord
 from dotenv import load_dotenv
+import os
 
 #load config
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+# sql server connection details
+
+#connect to discord
+#set intents
+intents = discord.Intents.default()
+
+bot_client = discord.Client(intents=intents)
+#fire off client using environmental token 
+bot_client.run(BOT_TOKEN)
