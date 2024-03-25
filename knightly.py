e78d2a431c9832064d55c6ed11bf1428b2d16ad1
Ryan James OSullivan <rjo@rynet.com>
2024-03-25 14:56:22 -0400

    added initial events

--- knightly.py
+++ knightly.py
@@ -10,7 +10,21 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 #connect to discord
 #set intents
 intents = discord.Intents.default()
+intents.message_content = True
 
 bot_client = discord.Client(intents=intents)
-#fire off client using environmental token 
+
+# register events
+@bot_client.event
+async def on_ready():
+    print(f'Logged in as {bot_client.user}')
+
+@bot_client.event
+async def on_message(message):
+    print(f'Message from {message.author}: {message.content}')
+
+    if message.content.startswith('Ping?'):
+        await message.channel.send('Pong!')
+
+#start up server w/ bot_token using environmental token 
 bot_client.run(BOT_TOKEN)
